5fdc07d981fc824807a68481049fff742bcd5dfc
Victor Svetuskin <vicsvetdev@gmail.com>
2021-03-11 19:45:05 +0200

    Adding owner index and controller. Closes #13.

--- pet-clinic-web/src/main/java/com/vicsvetdev/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/vicsvetdev/petclinic/controllers/OwnerController.java
@@ -3,12 +3,12 @@ package com.vicsvetdev.petclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/vets")
+@RequestMapping("/owners")
 @Controller
-public class VetController {
+public class OwnerController {
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
-        return "vets/index";
+    @RequestMapping({"", "/","/index", "/index.html"})
+    public String listOwners(){
+        return "owners/index";
     }
 }

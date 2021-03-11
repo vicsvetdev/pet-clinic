a9642fdbbeed0c4a2cd5a2767d8063952679d4bb
Victor Svetuskin <vicsvetdev@gmail.com>
2021-03-11 17:29:05 +0200

    Implemented map services. Closes #6.

---
+++ pet-clinic-data/src/main/java/com/vicsvetdev/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package com.vicsvetdev.petclinic.services.map;
+
+import com.vicsvetdev.petclinic.model.Vet;
+import com.vicsvetdev.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}

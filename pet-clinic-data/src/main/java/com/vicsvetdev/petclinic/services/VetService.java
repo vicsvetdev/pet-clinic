cdb7dbdac4f3ed26a360f7585c5c5bee3e89762c
Victor Svetuskin <vicsvetdev@gmail.com>
2021-03-11 12:20:32 +0200

    Created several services. Closes #5.

---
+++ pet-clinic-data/src/main/java/com/vicsvetdev/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.vicsvetdev.petclinic.services;
+
+import com.vicsvetdev.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}

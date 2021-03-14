1922f28a7c895e8924ebb1cca3737bfe0e6b31b4
Victor Svetuskin <vicsvetdev@gmail.com>
2021-03-14 10:40:37 +0200

    Added specialties map impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/vicsvetdev/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.vicsvetdev.petclinic.services;
+
+import com.vicsvetdev.petclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/vicsvetdev/petclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,36 @@
+package com.vicsvetdev.petclinic.services.map;
+
+import com.vicsvetdev.petclinic.model.Specialty;
+import com.vicsvetdev.petclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}

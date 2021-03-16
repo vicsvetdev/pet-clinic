badd5ba3eaa36f2ae70a71cf683d3566d329783a
Victor Svetuskin <vicsvetdev@gmail.com>
2021-03-16 18:52:25 +0200

    Removed builders from Owner and Person

--- pet-clinic-data/src/main/java/com/vicsvetdev/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/vicsvetdev/petclinic/model/Owner.java
@@ -15,15 +15,6 @@ import java.util.Set;
 @Table(name = "owners")
 public class Owner  extends Person{
 
-    @Builder
-    public Owner(Long id, String firstName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
-        super(id, firstName, lastName);
-        this.address = address;
-        this.city = city;
-        this.telephone = telephone;
-        this.pets = pets;
-    }
-
     @Column(name = "address")
     private String address;
     @Column(name = "city")
--- pet-clinic-data/src/main/java/com/vicsvetdev/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/vicsvetdev/petclinic/model/Person.java
@@ -1,6 +1,9 @@
 package com.vicsvetdev.petclinic.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
@@ -11,13 +14,6 @@ import javax.persistence.MappedSuperclass;
 @AllArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity{
-
-    @Builder
-    public Person(Long id, String firstName, String lastName) {
-        super(id);
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
 
     @Column(name = "first_name")
     private String firstName;

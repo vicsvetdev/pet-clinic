afae555dda062c327d1b51986be15cc07118b637
Victor Svetuskin <vicsvetdev@gmail.com>
2021-03-23 15:26:58 +0200

    Implemented find owner. Closes #54

--- pet-clinic-data/src/main/java/com/vicsvetdev/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/vicsvetdev/petclinic/repositories/OwnerRepository.java
@@ -3,7 +3,11 @@ package com.vicsvetdev.petclinic.repositories;
 import com.vicsvetdev.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/com/vicsvetdev/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/vicsvetdev/petclinic/services/OwnerService.java
@@ -2,8 +2,11 @@ package com.vicsvetdev.petclinic.services;
 
 import com.vicsvetdev.petclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
 
+    List<Owner> findAllByLastNameLike(String lastName);
 }
